827c334c325c96b8ed5e2711cc3a3cf4629c8167
Samet Serpil <samet.serpil@semper-tech.com>
2017-06-09 16:57:31 +0300

    added wallet adding fragment

---
+++ app/src/main/java/com/samet/ethermine/etherminepoolmonitor/fragments/AddNewWalletFragment.java
@@ -0,0 +1,45 @@
+package com.samet.ethermine.etherminepoolmonitor.fragments;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.samet.ethermine.etherminepoolmonitor.R;
+
+public class AddNewWalletFragment extends Fragment {
+    private Button mPlusOneButton;
+
+
+    public AddNewWalletFragment() {
+        // Required empty public constructor
+    }
+
+    public static AddNewWalletFragment newInstance(String param1, String param2) {
+        return new AddNewWalletFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_new_wallet, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+
+}
